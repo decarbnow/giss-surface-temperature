7f81c5cbfafc93f17ab30b5cd14fe8a5bd81b1b2
sweing <sjweingaertner@gmail.com>
2020-11-20 17:42:00 +0100

    remove bug in low temp

--- leafletPreview.r
+++ leafletPreview.r
@@ -11,14 +11,13 @@ loadPackages(c("leaflet",
 # SETUP
 # ----------------------------------------------
 list.files("tmp")
-jsonFile = "September2020_1951-1980.geojson"
+jsonFile = "August2020_1951-1980.geojson"
 # ----------------------------------------------
 
 # ----------------------------------------------
 # SHOW IN LEAFLET MAP
 # ----------------------------------------------
 polys = readOGR(file.path("tmp", jsonFile))
-poly_colors = c("brown", "purple", "red", "green", "blue", "yellow", "black", "gray", "brown", "orange", "purple", "brown")
 
 leaflet_map = leaflet() %>% addProviderTiles("CartoDB.Positron")
 
